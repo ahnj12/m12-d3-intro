8c97933d173a5fe344ad291f9576915f87469a06
Jooneil Ahn <jooneil.ahn@faira.com>
2017-04-11 13:23:40 -0700

    finished exercises 1 and 2

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -3,14 +3,26 @@
 // You'll have to wait for you page to load to assign events to the elements created in your index.html file
 $(function() {
 
-  // Select the element with id `sandbox` and append a `div` element to it. Make sure to store the div in a JavaScript varaible
-
-  // Create a variable `mySvg` by appending an `svg` element to your newly created `div`
-
-  // Set both the width and height attributes of your `svg` to 300 
-
-  // Append a `circle` element to your `svg`, setting the properties noted in index.html
-
-  // Append a `rect` element to your `svg`, setting the properties noted in index.html
-
+    // Select the element with id `sandbox` and append a `div` element to it. Make sure to store the div in a JavaScript variable
+    var sandbox = d3.select('#sandbox').append('div');
+    // Create a variable `mySvg` by appending an `svg` element to your newly created `div`
+    var mySvg = sandbox.append('svg');
+    // Set both the width and height attributes of your `svg` to 300
+    mySvg.attr('width', 300)
+        .attr('height', 300);
+    // Append a `circle` element to your `svg`, setting the properties noted in index.html
+    mySvg.append('circle')
+        .attr('r', 30)
+        .attr('cx', 75)
+        .attr('cy', 30)
+        .attr('fill', 'blue')
+        .style('opacity', 0.5);
+    // Append a `rect` element to your `svg`, setting the properties noted in index.html
+    mySvg.append('rect')
+        .attr('x', 70)
+        .attr('y', 60)
+        .attr('width', 10)
+        .attr('height', 250)
+        .attr('fill', 'orange')
+        .style('opacity', 0.5);
 });
